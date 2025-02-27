5e7591edce70e03e9c2fb0471efd27d5cb47a422
minnu2204 <tadikondasrivardhini9@gmail.com>
2025-02-27 15:13:51 +0530

    day3

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const app = express();
+const port =2204;
+
+
+
+app.get('/',(req,res)=>{
+    res.send('MINNU');
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
